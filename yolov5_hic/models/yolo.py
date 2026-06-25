abbfdc4bf1142f327ede8153eb7e45dea0e755d4
g111432006 <g111432006@grad.ntue.edu.tw>
2026-06-25 11:46:13 +0800

    dd

--- yolov5_hic/models/yolo.py
+++ yolov5_hic/models/yolo.py
@@ -48,6 +48,7 @@ from models.common import (
     GhostBottleneck,
     GhostConv,
     Proto,
+    CBAM,
 )
 from models.experimental import MixConv2d
 from utils.autoanchor import check_anchor_order
